427ada8f9c0b7e472b51a336e33c4a39d2ad6dbd
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2023-07-15 23:19:19 +0200

    insert sort

---
+++ src/main/java/section13/selectionMain.java
@@ -0,0 +1,44 @@
+package section13;
+
+import java.util.Arrays;
+
+public class selectionMain {
+
+    public static void main(String[] args) {
+
+        Integer[] input = {13,2,4,2,4,6,8,0,12,2};
+
+        System.out.println(Arrays.toString(input));
+
+        System.out.println(Arrays.toString(selectionSort(input)));
+
+
+    }
+
+    public static Integer[] selectionSort(Integer[] input){
+
+        Integer min;
+        int index;
+
+        for (int i = 0; i < input.length; i++) {
+            min=input[i];
+            index=i;
+
+            for(int j=i; j < input.length; j++){
+                if(min>input[j]){
+                    min=input[j];
+                    index=j;
+                }
+            }
+
+            if(min<input[i]){
+                input[index]=input[i];
+                input[i]=min;
+            }
+
+            System.out.println("Interation "+ i +" "+ Arrays.toString(input));
+        }
+        return input;
+    }
+
+}
